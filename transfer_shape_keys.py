53dfe721da8c0af2d7c717976d9096d1e0b73f44
Olli <ohihnala@gmail.com>
2019-02-21 21:51:30 +0200

    Add check if less than two shape keys exist

--- transfer_shape_keys.py
+++ transfer_shape_keys.py
@@ -8,8 +8,9 @@ bl_info = {
     "warning": "",
     "wiki_url": "",
     "category": "Object",
-    }
-	
+}
+
+
 import bpy
 
 
@@ -21,34 +22,37 @@ def main(self, context):
         for object in context.selected_objects:
             if object != target:
                 source = object
-                if not source.data.shape_keys:
-                    self.report({'INFO'},"No shape keys found to transfer")
-                    return
+                if not source.data.shape_keys or len(source.data.shape_keys.key_blocks) < 2:
+                    self.report({'INFO'}, "No shape keys found to transfer")
+                    return {'CANCELLED'}
     else:
-        self.report({'INFO'},"Selected two objects")
-        return
-    
+        self.report({'INFO'}, "Selected two objects")
+        return {'CANCELLED'}
+
     key_values = {}
     for key in source.data.shape_keys.key_blocks:
         key_values[key.name] = key.value
-    
+
     if source.data.shape_keys:
         keys = source.data.shape_keys.key_blocks[1:]
         for key in keys:
             key.value = 0
             bpy.context.scene.update()
-            modifier =target.modifiers.new(name=key.name, type='SURFACE_DEFORM')
+            modifier = target.modifiers.new(
+                name=key.name, type='SURFACE_DEFORM')
             modifier.target = source
             bpy.ops.object.surfacedeform_bind(modifier=modifier.name)
             key.value = 1
             bpy.context.scene.update()
-            bpy.ops.object.modifier_apply(apply_as='SHAPE', modifier=modifier.name)
+            bpy.ops.object.modifier_apply(
+                apply_as='SHAPE', modifier=modifier.name)
             key.value = 0
 
         for key in keys:
             print(key)
             key.value = key_values[key.name]
-            
+    return {'FINISHED'}
+
 
 class TransferShapeKeysBySurface(bpy.types.Operator):
     """Transfer shape keys between non-identical meshes"""
@@ -60,9 +64,12 @@ class TransferShapeKeysBySurface(bpy.types.Operator):
     def poll(cls, context):
         return context.active_object is not None
 
-    def execute(self,context):
-        main(self, context)
-        return {'FINISHED'}
+    def execute(self, context):
+        result = main(self, context)
+        if 'CANCELLED' in result:
+            return {'CANCELLED'}
+        else:
+            return {'FINISHED'}
 
 
 def add_to_object_menu(self, context):
@@ -77,6 +84,7 @@ def register():
 def unregister():
     bpy.utils.unregister_class(TransferShapeKeysBySurface)
     bpy.types.VIEW3D_MT_object.remove(add_to_object_menu)
+
 
 if __name__ == "__main__":
     register()
